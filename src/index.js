4e65080e0c0a86b536a617487c821b61b93c1ca8
MohammadAli-14 <rajaali8383679@gmail.com>
2025-05-07 06:36:51 -0700

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-import express from 'express'
+import express from "express";
 import "dotenv/config";
-import cors from 'cors';
-import job from './lib/cron.js';
+import cors from "cors";
+import job from "./lib/cron.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from './lib/db.js';
 const app=express();    
-//Creating the middleware here
-app.use(express.json());
 const PORT=process.env.PORT ||3000;
 
 job.start(); // Start the cron job
+//Creating the middleware here
+app.use(express.json());
+app.use(cors());
 app.use("/api/auth",authRoutes);
 app.use("/api/books",bookRoutes);
-app.use(cors());
-
 
 app.listen(PORT,()=>{
  console.log(`Server is listening on port:${PORT}`);
